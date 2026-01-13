f849493945338f89d3e97584ecfaf782d251c7e7
Dawlet04 <saymanovaguliy@gmail.com>
2026-01-13 13:39:00 +0500

    Update admin.py

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Roles, Users, Teachers, Payment, Enrollments, Categories, Courses,Lessons, Lesson_progress, Review
 
+admin.site.register(Roles)
+admin.site.register(Users)
+admin.site.register(Lesson_progress)
+admin.site.register(Lessons)
+admin.site.register(Enrollments)
+admin.site.register(Payment)
+admin.site.register(Teachers)
+admin.site.register(Categories)
+admin.site.register(Courses)
+admin.site.register(Review)
 # Register your models here.
